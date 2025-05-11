f34047b53a407c4b11b2125b3465d489ae914eb3
DI-NS <diegonunes967780439@gmail.com>
2025-05-10 22:59:29 -0300

    Corrigindo bug do get em /ubs/cnes/medicamentos

--- Ubs-Microserviço/src/main/java/Medmap/Ubs_Microservico/dto/MedicamentoDTO.java
+++ Ubs-Microserviço/src/main/java/Medmap/Ubs_Microservico/dto/MedicamentoDTO.java
@@ -1,11 +1,16 @@
 package Medmap.Ubs_Microservico.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Projeção recebida do microserviço de Medicamentos.
  */
 public record MedicamentoDTO(
         Long id,
         String nome,
+
+        @JsonProperty("informacoes")
         String descricao,
+
         boolean ativo
 ) {}
